52465f0a6378eb3705452185df46210a709150d9
Bapperida Lobar <bapperida_lobar@8902baba60c7>
2026-01-20 10:22:40 +0800

    update: absensi link

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 export const LINKS = {
     zoom: "https://us05web.zoom.us/j/83700430452?pwd=1kaHiMue5nbpZRV0ZamaaaUDkY2I6v.1",
-    absensi: "https://docs.google.com/forms/d/e/1FAIpQLSdrZx2S4Gc6nVRUcXrWzz041w0VqbjZ3c2puN5eaTIC_rbPVA/viewform?usp=sharing&ouid=100198688158729808624",
+    absensi: "https://near.tl/sm/Nd1jmJLUx",
     materi: "https://drive.google.com/drive/folders/1MdkPdBK9_5gvhlz7js_42SoJADF4KWPO?usp=drive_link",
     virtualBackground: "https://drive.google.com/file/d/1EIhr9uicXfi1bBvfLXRJQFxr21KhbQ-X/view?usp=drive_link",
     saran: "https://docs.google.com/forms/d/e/1FAIpQLSdj5MqIuq5mQRBCl7LRp5Oh0bI8Le0G35ktM2dX222a_WdvIg/viewform?usp=publish-editor"
